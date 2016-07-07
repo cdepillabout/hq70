051d5a699bd22dc0e49b0f699acf745ebf9e16f7
(cdep)illabout <cdep.illabout@gmail.com>
2016-07-07 21:42:16 +0900

    Completed problem 16.

--- questions/16/stairs/src/Lib.hs
+++ questions/16/stairs/src/Lib.hs
@@ -1,6 +1,33 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Prelude
+
+type NumOfStairs = Int
+
+type StepDist = Int
+type AStepDist = StepDist
+type BStepDist = StepDist
+
+type CurrentStair = Int
+type ACurrentStair = CurrentStair
+type BCurrentStair = CurrentStair
+
+type MeetStair = CurrentStair
+
+stepDistances :: [StepDist]
+stepDistances = [1,2,3,4]
+
+allCombinations :: NumOfStairs -> [Maybe MeetStair]
+allCombinations numOfStairs =
+    doMeet numOfStairs 0 numOfStairs
+
+doMeet :: NumOfStairs -> ACurrentStair -> BCurrentStair -> [Maybe MeetStair]
+doMeet numOfStairs aCurrentStair bCurrentStair
+    | aCurrentStair > bCurrentStair = pure Nothing
+    | aCurrentStair == bCurrentStair = pure $ Just aCurrentStair
+    | otherwise = do
+        aNextStepDist <- stepDistances
+        bNextStepDist <- stepDistances
+        let aNextStair = aCurrentStair + aNextStepDist
+            bNextStair = bCurrentStair - bNextStepDist
+        doMeet numOfStairs aNextStair bNextStair
